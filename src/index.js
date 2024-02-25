63dcad87496de93d68c861acff54facf493fbd3d
AndrewOPP <a.vyblov2003@gmail.com>
2024-02-25 18:33:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { store } from './redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/">
+      <BrowserRouter basename="/goit-test-task">
         <App />
       </BrowserRouter>
     </Provider>
